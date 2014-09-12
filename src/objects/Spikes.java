8f15411f8c8130b7dc3ab9c4e4001d905ad8895d
haiquangtran <hai.quang.tran@gmail.com>
2014-09-13 10:51:58 +1200

    Added ImageLibrary

--- src/objects/Spikes.java
+++ src/objects/Spikes.java
@@ -2,7 +2,12 @@ package objects;
 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import main.Constants;
 import tools.Animation;
 
 public class Spikes extends Tile{
@@ -11,8 +16,18 @@ public class Spikes extends Tile{
 		super(anim, x, y);
 	}
 
-	public static Spikes newUpSpikes(){
-		return null;
+	public static Spikes newUpSpikes(int x, int y){
+		BufferedImage img;
+		try {
+			img = ImageIO.read(new File(Constants.ASSETS + "test_tile.jpg"));
+			Animation anim = new Animation();
+			anim.addFrame(img, 0);
+			return new Spikes(anim,x,y);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public static Spikes newDownSpikes(){
