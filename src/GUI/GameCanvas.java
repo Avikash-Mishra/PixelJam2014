221d244209c426c986bcaf95621b01dcbc5ee719
Avikash-Mishra <avimish62@gmail.com>
2014-09-12 22:04:15 +1200

    Created canvas

---
+++ src/GUI/GameCanvas.java
@@ -0,0 +1,38 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+/**
+ * The display for the game
+ * @author Avikash Mishra
+ *
+ */
+public class GameCanvas extends Canvas {
+
+
+	private GameFrame parent;
+
+	/**
+	 * Constructs a new GameCanvas with the given width and height
+	 * @param width The Width of the canvas
+	 * @param height The Height of the canvas
+	 */
+	public GameCanvas(int width, int height, GameFrame parent) {
+		super(parent.getWidth(), parent.getHeight());
+		this.setBackground(Color.BLUE);
+		this.parent = parent;
+	}
+
+	/**
+	 * Draws everything.
+	 */
+	@Override
+	public void paint(Graphics g){
+
+	}
+
+
+
+}
