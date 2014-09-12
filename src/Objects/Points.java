2bae09e42b5e81634a1a5059231ee82e671e9b60
haiquangtran <hai.quang.tran@gmail.com>
2014-09-12 21:59:57 +1200

    draw methods

--- src/Objects/Points.java
+++ src/Objects/Points.java
@@ -1,6 +1,14 @@
 package Objects;
 
+import java.awt.image.BufferedImage;
+
 
 public class Points extends PickUpObject {
+
+	public Points(BufferedImage image, int x, int y, int amount) {
+		super(image, x, y, amount);
+		// TODO Auto-generated constructor stub
+	}
+
 
 }
