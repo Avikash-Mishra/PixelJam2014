1aba74701d8aeaa8146d2a0a0879b405704efd3a
haiquangtran <hai.quang.tran@gmail.com>
2014-09-12 21:54:13 +1200

    darw methods

--- src/Entity/CatEnemy.java
+++ src/Entity/CatEnemy.java
@@ -1,5 +1,12 @@
 package Entity;
 
+import java.awt.image.BufferedImage;
+
 public class CatEnemy extends Entity {
+
+	public CatEnemy(BufferedImage image, int x, int y) {
+		super(image, x, y);
+		// TODO Auto-generated constructor stub
+	}
 
 }
