ff32f09ce223df8e2c464caf8021a455368f811a
CraigTheLinguist <sinroth@gmail.com>
2014-09-13 13:29:58 +1200

    animations for player now cycle through

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -10,7 +10,7 @@ public class Player extends Entity {
 
 
 	private static final int STEP_SIZE = 10;
-	private static final long ANIMATION_DELAY = 500;
+	private static final long ANIMATION_DELAY = 100;
 
 	private Vector2D movement;
 	public int points = 0;
@@ -26,18 +26,19 @@ public class Player extends Entity {
 		animLeftWalking.addFrame(ImageLibrary.get("LcatWalk1Sprite.png"), ANIMATION_DELAY);
 		animLeftWalking.addFrame(ImageLibrary.get("LcatWalk2Sprite.png"), ANIMATION_DELAY);
 		animLeftWalking.addFrame(ImageLibrary.get("LcatStaticSprite.png"), ANIMATION_DELAY);
+		animLeftWalking.start();
 
 		animRightWalking = new Animation();
 		animRightWalking.addFrame(ImageLibrary.get("RcatWalk1Sprite.png"), ANIMATION_DELAY);
 		animRightWalking.addFrame(ImageLibrary.get("RcatWalk2Sprite.png"), ANIMATION_DELAY);
 		animRightWalking.addFrame(ImageLibrary.get("RcatStaticSprite.png"), ANIMATION_DELAY);
+		animRightWalking.start();
 
 		animLeftStatic = new Animation();
 		animLeftStatic.addFrame(ImageLibrary.get("LcatStaticSprite.png"), ANIMATION_DELAY);
 
 		animRightStatic = new Animation();
 		animRightStatic.addFrame(ImageLibrary.get("RcatStaticSprite.png"),ANIMATION_DELAY);
-
 	}
 
 	public Player(int x, int y) {
