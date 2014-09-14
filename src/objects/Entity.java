adab222d52faa5d7c63b9d17a67e46560c7a3da5
CraigTheLinguist <sinroth@gmail.com>
2014-09-14 13:54:13 +1200

    generalised checkBelow into check

--- src/objects/Entity.java
+++ src/objects/Entity.java
@@ -61,16 +61,16 @@ public abstract class Entity extends GameObject{
 		// move down the entire length of the movement vector or you collide
 		// wit something
 		while (
-				
+
 			distTravelled < distBetween // check you haven't travelled too far
 			&&
 			!(Utilities.colliding(new Rectangle( (int)pos.add(unit).x(),(int)pos.add(unit).y(),Constants.PLAYER_WIDTH,Constants.PLAYER_HEIGHT), nearby))) //check you haven't hit anything
-		{			
+		{
 			pos=pos.add(unit);
 			distTravelled++;
 		}
 		position = pos;
-	
+
 		// if you didn't move vertically, then you must have either
 		// hit the ground or you're at the maximum of the jumping parabola
 		// if you hit the ground, set velocity_y <-- 0
@@ -79,30 +79,12 @@ public abstract class Entity extends GameObject{
 				movement.setY(0);
 			}
 		}
-		
+
 	}
 
 	public void applyGravity(List<Tile> nearby){
 
-		// check the rectangle below your position that is PLAYER_WIDTH wide and 1 pixel high
-		Rectangle r = new Rectangle(getX(),getY()+Constants.PLAYER_HEIGHT+1,Constants.PLAYER_WIDTH,1);
-		boolean onGround = false;
-		for (Tile tile : nearby){
-			Rectangle bounding = tile.boundingBox();
-			
-			if (r.intersects(bounding)){
-				//Fall through water?
-				if (tile instanceof River){
-					onGround = false;
-					break;
-				}
-				onGround = true;
-				
-				break;
-			}
-		}
-
-		grounded = onGround;
+		grounded = checkBelow(nearby,Direction.BELOW);
 
 		if (!grounded && !movement.atTerminalVelocity()){
 			movement = movement.add(Constants.GRAVITY_VECTOR);
@@ -110,6 +92,54 @@ public abstract class Entity extends GameObject{
 
 	}
 
+	/**
+	 * Check a list of tiles and return true if something is below, left, right, or above this entity.
+	 * @param tilesToCheck: those tiles that should be checked. Get these by calling nearby method.
+	 * @param dir: direction to check
+	 * @return: list of tiles below, above, left, or right.
+	 */
+	private boolean checkBelow(List<Tile> tilesToCheck, Direction dir){
+
+		Rectangle r = null;
+		switch(dir){
+
+		case ABOVE:
+			r = new Rectangle(getX(),getY()-1,Constants.PLAYER_WIDTH,1);
+			break;
+		case LEFT:
+			r = new Rectangle(getX()-1,getY(),1,Constants.PLAYER_HEIGHT);
+			break;
+		case RIGHT:
+			r = new Rectangle(getX()+Constants.PLAYER_WIDTH+1,getY(),1,Constants.PLAYER_HEIGHT);
+			break;
+		case BELOW:
+			r = new Rectangle(getX(),getY()+Constants.PLAYER_HEIGHT+1,Constants.PLAYER_WIDTH,1);
+
+		}
+
+		for (Tile tile : tilesToCheck){
+			Rectangle bounding = tile.boundingBox();
+
+			if (r.intersects(bounding)){
+				//Fall through water?
+				if (tile instanceof River){
+					return false;
+				}
+				return true;
+			}
+		}
+		return false;
+
+	}
+
+	private enum Direction{
+		ABOVE,LEFT,RIGHT,BELOW;
+	}
+
+	/**
+	 * Return true if the point (x,y) is a non-transparent pixel on this entity's image.
+	 * @return boolean
+	 */
 	public boolean on(int x, int y){
 		return Utilities.isPixelTransparent(x, y, animation.getImage());
 	}
