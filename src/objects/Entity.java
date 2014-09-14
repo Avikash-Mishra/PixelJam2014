f5b9e5c833c7ce7f20bc1007108896baa1843099
CraigTheLinguist <sinroth@gmail.com>
2014-09-14 14:27:22 +1200

    Revert "Merge branch 'master' of https://github.com/Avikash-Mishra/PixelJam2014"
    This reverts commit a5e3e212e2720869dad1067b095c6d9659df3a51, reversing
    changes made to 697d3468d5157ec1e91126990a96801254447dc2.

--- src/objects/Entity.java
+++ src/objects/Entity.java
@@ -1,7 +1,6 @@
 package objects;
 
 import java.awt.Rectangle;
-import java.util.LinkedList;
 import java.util.List;
 
 import main.Constants;
@@ -18,7 +17,7 @@ public abstract class Entity extends GameObject{
 
 	protected boolean grounded = true;
 	protected Vector2D movement = new Vector2D(0,0);
-	private static final Vector2D JUMP_VECTOR = new Vector2D(0,-10f);
+	private static final Vector2D JUMP_VECTOR = new Vector2D(0,-10);
 
 	public Entity(int x, int y){
 		super(x,y);
@@ -62,16 +61,16 @@ public abstract class Entity extends GameObject{
 		// move down the entire length of the movement vector or you collide
 		// wit something
 		while (
-
+				
 			distTravelled < distBetween // check you haven't travelled too far
 			&&
 			!(Utilities.colliding(new Rectangle( (int)pos.add(unit).x(),(int)pos.add(unit).y(),Constants.PLAYER_WIDTH,Constants.PLAYER_HEIGHT), nearby))) //check you haven't hit anything
-		{
+		{			
 			pos=pos.add(unit);
 			distTravelled++;
 		}
 		position = pos;
-
+	
 		// if you didn't move vertically, then you must have either
 		// hit the ground or you're at the maximum of the jumping parabola
 		// if you hit the ground, set velocity_y <-- 0
@@ -80,14 +79,30 @@ public abstract class Entity extends GameObject{
 				movement.setY(0);
 			}
 		}
-
+		
 	}
 
 	public void applyGravity(List<Tile> nearby){
 
-		boolean[] surroundings = checkSurroundings(nearby);
-		// array[0] = north, array[1] = south, array[2] = east, array[3] = west
-		grounded = surroundings[1];
+		// check the rectangle below your position that is PLAYER_WIDTH wide and 1 pixel high
+		Rectangle r = new Rectangle(getX(),getY()+Constants.PLAYER_HEIGHT+1,Constants.PLAYER_WIDTH,1);
+		boolean onGround = false;
+		for (Tile tile : nearby){
+			Rectangle bounding = tile.boundingBox();
+			
+			if (r.intersects(bounding)){
+				//Fall through water?
+				if (tile instanceof River){
+					onGround = false;
+					break;
+				}
+				onGround = true;
+				
+				break;
+			}
+		}
+
+		grounded = onGround;
 
 		if (!grounded && !movement.atTerminalVelocity()){
 			movement = movement.add(Constants.GRAVITY_VECTOR);
@@ -95,44 +110,6 @@ public abstract class Entity extends GameObject{
 
 	}
 
-	/**
-	 * Check if you are touching a tile in your surroundings.
-	 * Return a 4-sized array: north, south, east, west.
-	 */
-	private boolean[] checkSurroundings(List<Tile> nearby){
-
-		Rectangle above = new Rectangle(getX(),getY()-1,Constants.PLAYER_WIDTH,1);
-		Rectangle left = new Rectangle(getX()-1,getY(),1,Constants.PLAYER_HEIGHT);
-		Rectangle right = new Rectangle(getX()+Constants.PLAYER_WIDTH+1,getY(),1,Constants.PLAYER_HEIGHT);
-		Rectangle below = new Rectangle(getX(),getY()+Constants.PLAYER_HEIGHT+1,Constants.PLAYER_WIDTH,1);
-
-		boolean north, south, east, west;
-		north = south = east = west = false;
-
-		for (Tile tile : nearby){
-			Rectangle bounding = tile.boundingBox();
-
-			if (below.intersects(bounding)){
-				if (!(tile instanceof River)) south = true;
-			}
-			else if (above.intersects(bounding)){
-				if (!(tile instanceof River)) north = true;
-			}
-			else if (right.intersects(bounding)){
-				if (!(tile instanceof River)) east = true;
-			}
-			else if (left.intersects(bounding)){
-				if (!(tile instanceof River)) west = true;
-			}
-
-		}
-		return new boolean[]{ north, south, east, west };
-	}
-
-	/**
-	 * Return true if the point (x,y) is a non-transparent pixel on this entity's image.
-	 * @return boolean
-	 */
 	public boolean on(int x, int y){
 		return Utilities.isPixelTransparent(x, y, animation.getImage());
 	}
